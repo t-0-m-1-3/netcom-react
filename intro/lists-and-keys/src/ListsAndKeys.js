42de084a2bf13c919d5671eed588b9768315ded9
t0m <t0m@localhost.localdomain>
2019-10-24 09:10:15 -0400

    added in listing example

---
+++ intro/lists-and-keys/src/ListsAndKeys.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
+
+export default class ListsAndKeys extends React.Component{
+	render() {
+  return (
+    <div className="lists-and-keys">
+	  <ul>{listItems}</ul>
+    </div>
+  );
+}
+}
